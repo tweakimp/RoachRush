49044f26fd951fa9018eaefccf8e180e97e1f1c3
FW <tweakimp@gmail.com>
2019-05-04 12:01:37 +0200

    Update library, group army, focus pylons

--- Main.py
+++ Main.py
@@ -1,10 +1,8 @@
 import itertools
 import random
-
 import sc2
 from sc2.ids.ability_id import AbilityId as AbilID
 from sc2.ids.unit_typeid import UnitTypeId as UnitID
-from sc2.position import Point2
 
 
 class RoachRush(sc2.BotAI):
@@ -42,8 +40,15 @@ class RoachRush(sc2.BotAI):
         self.larvae = None
         self.queens = None
         self.army = None
+        # flag we wave in case we want to give up
+        self.surrendered: bool = False
+        # expansions ordered by distance from starting location
+        self.ordered_expansions = None
 
     async def on_step(self, iteration):
+        # dont do anything if we surrendered already
+        if self.surrendered:
+            return
         # create selections one time for the whole frame
         # so that we dont have to filter the same units multiple times
         self.set_unit_groups()
@@ -54,6 +59,7 @@ class RoachRush(sc2.BotAI):
         if not self.drones:
             # surrender phrase for ladder manager
             await self.chat_send("(pineapple)")
+            self.surrendered = True
             return
         await self.do_buildorder()
         await self.inject()
@@ -61,9 +67,9 @@ class RoachRush(sc2.BotAI):
         # buildorder completed, start second phase of the bot
         if self.buildorder[self.buildorder_step] == "END":
             self.build_army()
-            self.send_idle_army()
-            self.control_fighting_army()
-            self.additional_overlords()
+            self.build_additional_overlords()
+            self.set_army_target()
+            self.control_army()
         # do list of actions of the current step
         await self.do_actions(self.actions)
         # empty list to be ready for new actions in the next frame
@@ -83,6 +89,10 @@ class RoachRush(sc2.BotAI):
             # find closest mineral patch
             closest_mineral_patch = self.state.mineral_field.closest_to(drone)
             self.actions.append(drone.gather(closest_mineral_patch))
+        # prepare ordered expansions, sort by distance to start location
+        self.ordered_expansions = sorted(
+            self.expansion_locations.keys(), key=lambda expansion: expansion.distance_to(self.start_location)
+        )
         # only do on_step every nth step, 8 is standard
         self._client.game_step = 8
 
@@ -176,49 +186,39 @@ class RoachRush(sc2.BotAI):
             elif self.minerals >= 50 and self.vespene <= 8:
                 self.actions.append(self.larvae.first.train(UnitID.ZERGLING))
 
-    def send_idle_army(self):
-        # we can only fight ground units and we dont want to fight larvae
-        ground_enemies = self.known_enemy_units.filter(lambda unit: not unit.is_flying and unit.type_id != UnitID.LARVA)
-        # we dont see anything, go to enemy start location (only works on 2 player maps)
-        if not ground_enemies:
-            # if we didnt start to clear the map already
-            if not self.clear_map:
-                # start with enemy starting location, then cycle through all expansions
-                self.clear_map = itertools.cycle(
-                    [self.enemy_start_locations[0]] + list(self.expansion_locations.keys())
-                )
-                self.army_target = next(self.clear_map)
-            # we can see the expansion but there seems to be nothing, get next
-            if self.units.closer_than(7, self.army_target):
-                self.army_target = next(self.clear_map)
-            # send all units
-            for unit in self.army:
-                self.actions.append(unit.move(self.army_target))
-        else:
-            # select only idle units, the other units have tasks already
-            army_idle = self.army.idle
-            # wait with first attack until we have 5 units
-            if army_idle.amount < 6:
-                return
-            # send all units
-            for unit in army_idle:
-                # attack closest unit
-                closest_enemy = ground_enemies.closest_to(unit)
-                self.actions.append(unit.attack(closest_enemy))
+    def set_army_target(self):
+        # sets the next waypoint for the army in case there is nothing on the map
+        # if we didnt start to clear the map already
+        if not self.clear_map:
+            # start with enemy starting location, then cycle through all expansions
+            self.clear_map = itertools.cycle(reversed(self.ordered_expansions))
+            self.army_target = next(self.clear_map)
+        # we can see the expansion but there seems to be nothing there, get next
+        if self.units.closer_than(6, self.army_target):
+            self.army_target = next(self.clear_map)
 
-    def control_fighting_army(self):
+    def control_army(self):
+        # calculate actions for the army units
+        army = self.units.filter(lambda unit: unit.type_id in {UnitID.ROACH, UnitID.ZERGLING})
+        # dont do anything if we dont have an army
+        if not army:
+            return
         # we can only fight ground units and we dont want to fight larvae
         ground_enemies = self.known_enemy_units.filter(lambda unit: not unit.is_flying and unit.type_id != UnitID.LARVA)
-        # no need to do anything here if we dont see anything
+        # we dont see anything so start to clear the map
         if not ground_enemies:
+            for unit in army:
+                self.actions.append(unit.attack(self.army_target))
             return
-        army = self.units.filter(lambda unit: unit.type_id in {UnitID.ROACH, UnitID.ZERGLING})
+        # calculate army size and center for grouping
+        army_size = len(army)
+        army_center = army.center
         # create selection of dangerous enemy units.
         # bunker and uprooted spine dont have weapon, but should be in that selection
-        # also add spinecrawler and cannon if they are not ready yet and have no weapon
+        # also add uprooted spinecrawler and bunker because they have no weapon and pylon to unpower protoss structures
         enemy_fighters = ground_enemies.filter(
             lambda u: u.can_attack
-            or u.type_id in {UnitID.BUNKER, UnitID.SPINECRAWLERUPROOTED, UnitID.SPINECRAWLER, UnitID.PHOTONCANNON}
+            or u.type_id in {UnitID.BUNKER, UnitID.SPINECRAWLERUPROOTED, UnitID.SPINECRAWLER, UnitID.PYLON}
         )
         for unit in army:
             if enemy_fighters:
@@ -233,24 +233,35 @@ class RoachRush(sc2.BotAI):
                     if unit.ground_range > 1:
                         # attack if weapon not on cooldown
                         if unit.weapon_cooldown == 0:
+                            # focus down pylons in range
+                            pylons = in_range_enemies(UnitID.PYLON)
+                            if pylons:
+                                in_range_enemies = pylons
                             # attack enemy with lowest hp of the ones in range
                             lowest_hp = min(in_range_enemies, key=lambda e: e.health + e.shield)
                             self.actions.append(unit.attack(lowest_hp))
                         else:
                             closest_enemy = enemy_fighters.closest_to(unit)
-                            self.actions.append(unit.move(closest_enemy.position.towards(unit, unit.ground_range)))
+                            # micro awy from closest unit, distance one shorter than range
+                            # to let other friendly units get close enough as well and not block
+                            self.actions.append(unit.move(closest_enemy.position.towards(unit, unit.ground_range - 1)))
                     else:
-                        # target fire with lings
+                        # target fire with melee units
                         lowest_hp = min(in_range_enemies, key=lambda e: e.health + e.shield)
                         self.actions.append(unit.attack(lowest_hp))
                 else:
-                    # no unit in range, go to closest
-                    self.actions.append(unit.move(enemy_fighters.closest_to(unit)))
+                    # no unit in range, go to closest enemy if unit near army center
+                    # depending on the army size
+                    if unit.distance_to(army_center) < army_size ** 1.5 - army_size ** 1.2:
+                        self.actions.append(unit.move(enemy_fighters.closest_to(unit)))
+                    # no unit in range, and unit is not near army center, group up
+                    else:
+                        self.actions.append(unit.move(army_center))
+            # no dangerous enemy at all, attack closest anyhting
             else:
-                # no dangerous enemy at all, attack closest of everything
                 self.actions.append(unit.attack(ground_enemies.closest_to(unit)))
 
-    def additional_overlords(self):
+    def build_additional_overlords(self):
         # build more overlords after buildorder
         # you need larva and enough minerals
         # prevent overlords if you have reached the cap already
@@ -265,12 +276,15 @@ class RoachRush(sc2.BotAI):
 
 
 def main():
+    # create bot instance
     bot = sc2.player.Bot(sc2.Race.Zerg, RoachRush())
     # fixed race seems to use different strats than sc2.Race.Random
+    # choose a race for the opponent builtin bot
     race = random.choice([sc2.Race.Zerg, sc2.Race.Terran, sc2.Race.Protoss, sc2.Race.Random])
+    # choose a strategy for the opponent builtin bot
     build = random.choice(
         [
-            # sc2.AIBuild.RandomBuild,
+            sc2.AIBuild.RandomBuild,
             sc2.AIBuild.Rush,
             sc2.AIBuild.Timing,
             sc2.AIBuild.Power,
@@ -278,19 +292,22 @@ def main():
             sc2.AIBuild.Air,
         ]
     )
+    # create the opponent builtin bot instance
     builtin_bot = sc2.player.Computer(race, sc2.Difficulty.VeryHard, build)
+    # choose a random map
     random_map = random.choice(
         [
             "AutomatonLE",
-            # "BlueshiftLE",
-            # "CeruleanFallLE",
-            # "KairosJunctionLE",
-            # "ParaSiteLE",
-            # "PortAleksanderLE",
-            # "StasisLE",
-            # "DarknessSanctuaryLE"  # 4 player map, bot is ready for it but has to find enemy first
+            "BlueshiftLE",
+            "CeruleanFallLE",
+            "KairosJunctionLE",
+            "ParaSiteLE",
+            "PortAleksanderLE",
+            "StasisLE",
+            "DarknessSanctuaryLE",  # 4 player map, bot is ready for it but has to find enemy first
         ]
     )
+    # start the game with both bots
     sc2.run_game(sc2.maps.get(random_map), [bot, builtin_bot], realtime=False)
 
 
